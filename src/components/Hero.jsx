9e40e00225bf022a7c339c54f90eeea0f8191975
Ghulam Murtaza <murtazakasur7@gmail.com>
2026-04-18 21:01:55 +0500

    Hero section Buttons error fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ function Hero() {
 
             {/* Content */}
             <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center text-white min-h-screen">
-                <div className="flex items-center justify-center flex-col space-y-5 p-25 animate-fadeInUp min-h-screen">
+                <div className="flex items-center justify-center flex-col space-y-5 py-25 px-18 animate-fadeInUp min-h-screen">
                     {/* Badge */}
                     <div className="flex items-center justify-center space-x-2">
                         <div className="flex items-center bg-white/10 backdrop-blur-sm px-4 py-2 rounded-full">
@@ -56,7 +56,7 @@ function Hero() {
                     <div className="flex flex-col sm:flex-row items-center justify-center gap-4 pt-4">
                         <Link
                             to="/packages"
-                            className="group bg-[#C8A96A] text-white px-8 py-4 rounded-full text-lg font-medium hover:bg-[#B89B5A] transition-all hover:scale-105 shadow-xl flex items-center space-x-2"
+                            className="group bg-[#C8A96A] text-white w-full md:w-auto px-8 py-4 rounded-full text-lg font-medium hover:bg-[#B89B5A] transition-all hover:scale-105 shadow-xl flex items-center space-x-2"
                         >
                             <span>Explore Packages</span>
                             <ArrowRight className="w-5 h-5 group-hover:translate-x-1 transition-transform" />
@@ -64,7 +64,7 @@ function Hero() {
 
                         <Link
                             to="/services"
-                            className="bg-white/10 backdrop-blur-sm text-white px-8 py-4 rounded-full text-lg font-medium border border-white/20 hover:bg-white/20 transition-all hover:scale-105"
+                            className="bg-white/10 w-full md:w-auto backdrop-blur-sm text-white px-8 py-4 rounded-full text-lg font-medium border border-white/20 hover:bg-white/20 transition-all hover:scale-105"
                         >
                             View Services
                         </Link>
